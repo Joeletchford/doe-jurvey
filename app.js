ebba2727d962caeeefd407caabcbfe423d10026d
Joe <joe@150-253-89-233.wifi.mcad.edu>
2016-09-23 15:18:52 -0500

    sound and delay

--- app.js
+++ app.js
@@ -3,24 +3,55 @@
 //
 // $( ".chat-body").append( "<h2>fuck u</h2>" );
 
-var joeIM = function(){
-    $().ready( "<audio autoplay='true'>" +
-    "<source src='sounds/ReceiveIM.wav' type='audio/mpeg'>" + "</audio>");
-}
-
-var danielIM = function(){
-    $().ready( "<audio autoplay='true'>" +
-    "<source src='sounds/SendIM.wav' type='audio/mpeg'>" + "</audio>");
-}
 
 $(document).ready(function () {
     setTimeout(function(){
-        $( ".chat-body").append("<h1>Hey Daniel! How was school today?</h1>" + joeIM).fadeIn(500);
+        $( ".chat-body").append("<h1>Hey Daniel! How was school today?</h1>").fadeIn(500);
+        $(document).ready(function () {
+                  var joeIM = document.createElement('audio');
+                  joeIM.setAttribute('src', 'sounds/ReceiveIM.wav');
+                  joeIM.setAttribute('autoplay', 'autoplay');
+                  //audioElement.load()
+                  $.get();
+                  joeIM.addEventListener(".chat-body", function() {
+                  joeIM.play();
+                  }, true);
+                });
+      $(".chat-body").scrollTop($('.chat-body').height())
     }, 3000);
 });
 
 $(document).ready(function () {
     setTimeout(function(){
-        $( ".chat-body").append( "<h2>AYy it was alright</h2>" + danielIM).fadeIn(500);
+        $( ".chat-body").append( "<h2>AYy it was alright</h2>" ).fadeIn(500);
+        $(document).ready(function() {
+          var danielIM = document.createElement('audio');
+          danielIM.setAttribute('src', 'sounds/SendIM.wav');
+          danielIM.setAttribute('autoplay', 'autoplay');
+          //audioElement.load()
+          $.get();
+          danielIM.addEventListener(".chat-body", function() {
+          danielIM.play();
+        }, true);
+        });
+        $(".chat-body").scrollTop($('.chat-body').height())
     }, 5000);
+});
+
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $( ".chat-body").append("<h1>mannnn Ben's class is super rough! Coding can take a lap.</h1>").fadeIn(500);
+        $(document).ready(function () {
+                  var joeIM = document.createElement('audio');
+                  joeIM.setAttribute('src', 'sounds/ReceiveIM.wav');
+                  joeIM.setAttribute('autoplay', 'autoplay');
+                  //audioElement.load()
+                  $.get();
+                  joeIM.addEventListener(".chat-body", function() {
+                  joeIM.play();
+                  }, true);
+                });
+                      $(".chat-body").scrollTop($('.chat-body').height())
+    }, 8000);
 });
