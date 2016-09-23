05500e670bfcbf441e657663f0d0001906aee067
Joe <joe@150-253-89-233.wifi.mcad.edu>
2016-09-23 13:21:13 -0500

    hey daniel

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-//h1 is Joe, h2 is daniel
-$( ".chat-body").append( "<h1>ayyo bruh</h1>" );
+// //h1 is Joe, h2 is daniel
+// $( ".chat-body").append( "<h1>ayyo bruh</h1>" );
+//
+// $( ".chat-body").append( "<h2>fuck u</h2>" );
 
-$( ".chat-body").append( "<h2>fuck u</h2>" );
 
-$( ".chat-body").append( "<h2>hey amn</h2>" );
-$( ".chat-body").append( "<h2>hey amn</h2>" );
 
-$( ".chat-body").append( "<h2>hey amn</h2>" );
 
-$( ".chat-body").append( "<h2>hey amn</h2>" );
+$(document).ready(function () {
+    setTimeout(function(){
+        $( ".chat-body").append( "<h1>Hey Daniel! How was school today?</h1>" ).fadeIn(500);
+    }, 3000);
+});
 
-$( ".chat-body").append( "<h2>hey amn</h2>" );
-
-$( ".chat-body").append( "<h2>hey amn</h2>" );
-
-$( ".chat-body").append( "<h2>hey amn</h2>" );
-
-$( ".chat-body").append( "<h2>hey amn</h2>" );
-
-$( ".chat-body").append( "<h2>hey amn</h2>" );
+$(document).ready(function () {
+    setTimeout(function(){
+        $( ".chat-body").append( "<h2></h2>" ).fadeIn(500);
+    }, 5000);
+});
