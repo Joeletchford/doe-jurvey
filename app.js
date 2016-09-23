fbf65068d91940bd9b265e03cda45737119d8c18
Daniel Lurvey <lurvey@150-253-89-246.wifi.mcad.edu>
2016-09-23 12:47:06 -0500

    added IMs and sound

---
+++ app.js
@@ -0,0 +1,21 @@
+//h1 is Joe, h2 is daniel
+$( ".chat-body").append( "<h1>ayyo bruh</h1>" );
+
+$( ".chat-body").append( "<h2>fuck u</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
+
+$( ".chat-body").append( "<h2>hey amn</h2>" );
